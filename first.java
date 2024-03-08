4bc0a5c16347bd337e123954b161629e935fb5e6
Susucorn <127116788+Susucorn@users.noreply.github.com>
2024-03-08 14:05:50 +0900

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+/*
+ 2024.03.08.Fri
+ 신라대학교 202395016 컴퓨터공학부 박수연
+ 설명 : 첫 번째 자바 프로그래밍
+ */
+public class first {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		System.out.print("박수연 입니다.");
+		System.out.println("반갑습니다." + "첫번째 자바 프로그램 입니다.");
+	}
+}
